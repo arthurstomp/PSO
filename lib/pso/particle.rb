7800a43ceb07dd4570e0cc6c8ac1d0634e467172
Arthur Rocha de Menezes <arthurstomp@gmail.com>
2013-09-03 17:01:40 -0300

    add more features to particle.

--- lib/pso/particle.rb
+++ lib/pso/particle.rb
@@ -1,4 +1,28 @@
 require File.join(File.dirname(__FILE__),'best')
 class Particle 
+  @@min = -5.0
+  @@max = 5.0
+  @@n_dimensions = 2
 
+  attr_accessor :position
+  def initialize
+    self.position = self.class.random_position(2)
+  end
+
+  def self.random_position(n_dimensions)
+    random_position = Array.new(n_dimensions)
+    n_dimensions.times do |i|
+      position_i = rand(@@max)
+      position_i *= rand > 0.5 ? 1 : -1
+      random_position[i-1] = position_i
+    end
+    random_position
+  end
+  
+  def self.n_dimensions
+    @@n_dimensions
+  end
+  def self.n_dimensions=(new_n_dimensions)
+    @@n_dimensions = new_n_dimensions
+  end
 end
--- spec/pso/particle_spec.rb
+++ spec/pso/particle_spec.rb
@@ -3,4 +3,37 @@ describe Particle do
   it 'should initialize' do
     expect{Particle.new}.not_to raise_error
   end
+
+  it 'class should generate a random position' do
+    Particle.random_position(2).should be_instance_of Array
+    Particle.random_position(2).size.should eq(2)
+  end
+
+  context 'class attribute n_dimensions' do
+    it 'should have the number of dimensions where it will fly' do
+      Particle.n_dimensions.should be_instance_of Fixnum
+      Particle.n_dimensions.should eq(2)
+      p1 = Particle.new
+      p2 = Particle.new
+      p1.class.n_dimensions.should eq(2)
+      p2.class.n_dimensions.should eq(2)
+    end
+    it 'should be able to set the number of dimensions of the class' do
+      p1 = Particle.new
+      p2 = Particle.new
+      Particle.n_dimensions = 3
+      Particle.n_dimensions.should eq(3)
+      p1.class.n_dimensions.should eq(3)
+      p2.class.n_dimensions.should eq(3)
+    end
+  end
+
+  context 'position attribute' do
+    before(:each) do 
+      @particle = Particle.new
+    end
+    it 'should have a position' do
+      @particle.position.should be_instance_of Array
+    end
+  end
 end
